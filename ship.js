9c787f76d8566d5ec79f6efbbb0acf48a38b4f13
Romain Dardour <romain@hull.io>
2015-03-31 14:53:12 +0200

    Updates

--- ship.js
+++ ship.js
@@ -1,7 +1,7 @@
-!function(e,t){"object"==typeof exports&&"object"==typeof module?module.exports=t():"function"==typeof define&&define.amd?define(t):"object"==typeof exports?exports["Hull comments count"]=t():e["Hull comments count"]=t()}(this,function(){return function(e){function t(o){if(n[o])return n[o].exports;var r=n[o]={exports:{},id:o,loaded:!1};return e[o].call(r.exports,r,r.exports,t),r.loaded=!0,r.exports}var n={};return t.m=e,t.c=n,t.p="/",t(0)}([/*!*********************!*\
+!function(e,t){"object"==typeof exports&&"object"==typeof module?module.exports=t():"function"==typeof define&&define.amd?define(t):"object"==typeof exports?exports["Hull Comments Count"]=t():e["Hull Comments Count"]=t()}(this,function(){return function(e){function t(o){if(n[o])return n[o].exports;var r=n[o]={exports:{},id:o,loaded:!1};return e[o].call(r.exports,r,r.exports,t),r.loaded=!0,r.exports}var n={};return t.m=e,t.c=n,t.p="/",t(0)}([/*!*********************!*\
   !*** ./src/ship.js ***!
   \*********************/
 function(e,t,n){"use strict";var o=function(e){return e&&e.__esModule?e["default"]:e},r=o(n(/*! ./bootstrap */1));Hull.onEmbed(document,r)},/*!**************************!*\
   !*** ./src/bootstrap.js ***!
   \**************************/
-function(e){"use strict";function t(e,t){var n=Hull.entity.encode(Hull.findUrl());Hull.api(n,function(n){var o,r=n.stats.comments||0;o=r>1?"more_comment":1===r?"one_comment":"zero_comment";var c=t.ship.translations.en[o].replace("{count}",r);e.innerHTML=c})}e.exports=t}])});
+function(e,t,n){"use strict";function o(e,t){var n=Hull.entity.encode(Hull.findUrl());Hull.api(n,function(n){var o,r=n.stats.comments||0;o=r>1?"more_comment":1===r?"one_comment":"zero_comment";var u=t.ship.translations.en[o].replace("{count}",r);e.innerHTML=u})}e.exports=o}])});
